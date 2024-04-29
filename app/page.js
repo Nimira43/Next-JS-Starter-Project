71f1659dcaf0b7f8773a975596a8472aba80d88b
Nick Rabone <n.rabone72@aol.com>
2024-04-29 22:09:24 +0100

    Customised boilerplate

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <img src="/logo.png" alt="NimiraTech Happy Bee!" />
+      <h1>Next JS </h1>
+      <p>Here We Go!!!</p>
     </main>
   );
 }
